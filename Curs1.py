37d19f2190107c9d4dda5ee43116a08b0b34dfa9
cojocaruconstantin03 <crysty.01103@yahoo.com>
2023-04-21 19:19:16 +0300

    tema

--- Curs1.py
+++ Curs1.py
@@ -1,4 +1,27 @@
-print("hello wordls")
-print("hello world")
+lista_me = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 
-print("hello wofdfsdfsdrld")
+# Afișarea listei inițiale
+print("Lista inițială:", lista_me)
+
+# Afișarea listei ordonate ascendent
+lista_asc = sorted(lista_me)
+print("Lista ordonată ascendent:", lista_asc)
+
+# Afișarea listei ordonate descendent
+lista_desc = sorted(lista_me, reverse=True)
+print("Lista ordonată descendent:", lista_desc)
+
+# Afișarea numerelor cu indici pari din listă
+numere_pare = lista_me[::2]
+print("Numerele cu indici pari din listă:", numere_pare)
+
+# Afișarea numerelor cu indici impari din listă
+numere_impare = lista_me[1::2]
+print("Numerele cu indici impari din listă:", numere_impare)
+
+# Afișarea elementelor multipli ai lui 3
+multipli_3 = []
+for elem in lista_me:
+    if elem % 3 == 0:
+        multipli_3.append(elem)
+print("Elementele multipli ai lui 3:", multipli_3)
